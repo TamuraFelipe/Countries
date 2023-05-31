596354452a06d9516d41e86a7611eec54b84eb01
Tamura <tamurafelipe@gmail.com>
2023-05-31 10:49:07 -0300

    useFetch at Home

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,12 +1,36 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { useCallback, useState } from 'react'
+import { api } from '../services/api'
 
-const useFetch = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const UseFetch = () => {
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    
+    const request = useCallback( async (url) => {
+        let json;
+
+        try {
+            setError(null);
+            setLoading(true);
+            const response = await api.get(url);
+            json = await response.data;
+            console.log(json)
+        } catch (error) {
+            json = null;
+            setError(error.message)
+        } finally {
+            setData(json)
+            setLoading(false)
+        }
+    }, [])
+  
+  return {
+    request,
+    data,
+    error,
+    loading,
+  }
 }
 
-export default useFetch
+export default UseFetch
